4b8824acf3ccd90cdb84920639730935d0fa6211
Gustavo González <gustavo.gonzalez@arauco.com>
2024-01-24 08:18:43 -0300

    backup

---
+++ main.js
@@ -0,0 +1,61 @@
+const { app, BrowserWindow } = require('electron')
+const { createClient } = require('@supabase/supabase-js')
+const supabaseUrl = 'https://ftxxcnmgoyrppxvjjcmr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0eHhjbm1nb3lycHB4dmpqY21yIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNDgyOTAzOCwiZXhwIjoyMDIwNDA1MDM4fQ.pJABUWJmVcrND8a-yGcJy-JeTjvKAUqz1CMaxiNy0AQ'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let mainWindow
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 1280, height: 720 })
+
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+    //mainWindow.webContents.openDevTools()
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
+
+async function testConection(){
+    try{
+        const { data, error } = await supabase.from('informe').select('observaciones_rutina');  
+        if(error){
+            console.error('Error de conexion', error.message)
+            return;    
+        }
+        console.log(data)
+    }
+    catch (error) {
+        console.error('Error: ',error.message)
+    }
+}
+
+testConection()
