35b34e10a98b5cd6d2b4de71ab5141a52ceba1ac
gugonzalez1409 <gustavo-gonzalez1409@hotmail.com>
2024-01-24 23:48:40 -0300

    refactor index.html

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let mainWindow
 function createWindow() {
     mainWindow = new BrowserWindow({ width: 1280, height: 720 })
     mainWindow.loadFile('index.html')
-    //mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
@@ -29,16 +29,12 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
 app.on('activate', function() {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow()
     }
---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', (event) => {
+    const pages = [
+        'areas.html',
+        'admin.html',
+        'adminCuentas.html',
+        'adminTrab.html',
+        'estadisticas.html',
+        'login.html',
+        'modRutinas.html',
+        'newRutina.html',
+        'user.html',
+    ];
+
+    const loadPage = async (page) => {
+        try {
+            const response = await fetch(`pages/${page}`);
+            const html = await response.text();
+            const div = document.createElement('div');
+            div.innerHTML = html;
+            const template = div.querySelector('template');
+            const clone = document.importNode(template.content, true);
+            const targetTab = document.getElementById(page.replace('.html',''));
+            if (targetTab) {
+                targetTab.appendChild(clone);
+                console.log(`Página ${page} cargada correctamente.`);
+            } else {
+                console.error(`Error: Contenedor para ${page} no encontrado.`);
+            }
+            const script = div.querySelector('script');
+            if (script) {
+                const scriptElement = document.createElement('script');
+                scriptElement.innerHTML = script.innerHTML;
+                targetTab.body.appendChild(scriptElement);
+                console.log(`Script de ${page} cargado correctamente.`);
+            }
+        } catch (error) {
+            console.error(`Error al cargar la página ${page}: ${error}`);
+        }
+    };
+    pages.forEach((page) => {
+        console.log(`Cargando página: ${page}`);
+        loadPage(page);
+        });
+    });
