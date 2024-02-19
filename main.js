c249880c310cc6263f4f85022d3bff53562c37bf
Gustavo González <gustavo.gonzalez@arauco.com>
2024-02-19 11:22:21 -0300

    eliminacion url supa

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { createClient } = require('@supabase/supabase-js');
 const path = require('path');
 const { event } = require('jquery');
 const main = require('electron-reload');
-const supabaseUrl = 'https://ftxxcnmgoyrppxvjjcmr.supabase.co';
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
